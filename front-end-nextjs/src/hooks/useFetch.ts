0623402588ff095e76a698e6969d4ba9171a24a4
shorif <shorifpatwary999@gmail.com>
2023-09-28 08:04:32 +0600

    [useFetch]

---
+++ front-end-nextjs/src/hooks/useFetch.ts
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+
+type HTTPMethod = "GET" | "POST" | "PUT" | "DELETE";
+
+interface UseFetchResponse<T> {
+  data: T | null;
+  error: string | null;
+  isLoading: boolean;
+  isComplete: boolean;
+}
+
+interface RequestOptions {
+  headers?: { [key: string]: string };
+  body?: any;
+}
+
+function useFetch<T>(
+  url: string,
+  method: HTTPMethod = "GET",
+  options: RequestOptions = {}
+): UseFetchResponse<T> {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [isComplete, setIsComplete] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setIsComplete(false);
+      setError(null);
+
+      try {
+        const response = await fetch(url, {
+          method,
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            ...options.headers,
+          },
+          body: JSON.stringify(options.body),
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          setError(`HTTP Error: ${response.statusText}`);
+        } else {
+          setData(responseData);
+        }
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+        setIsComplete(true);
+      }
+    };
+
+    fetchData();
+  }, [url, method, options.headers, options.body]);
+
+  return { data, error, isLoading, isComplete };
+}
+
+export default useFetch;
